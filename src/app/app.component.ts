f72e288ac65562ce8cee450843ff527556ff1af0
Sheroz Akram <sheroz.akram@reporteq.com>
2024-11-15 09:47:26 +0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TasksComponent ],
+  imports: [HeaderComponent, TasksComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
